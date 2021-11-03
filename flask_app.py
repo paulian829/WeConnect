e105cc6fd28fbb01d87c3286460dc33526451f1d
Paul Ian <paulian829@gmail.com>
2021-11-03 23:59:32 +0800

    Updates

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,5 @@
 from enum import unique
-from flask import Flask, render_template,request,json
+from flask import Flask, render_template,request,json,redirect,url_for
 from database import registerNewUser
 
 app = Flask(__name__)
@@ -12,10 +12,10 @@ def login():
         password = request.form.get('Password')
         print(email)
         print(password)
+        return redirect(url_for('home'))
 
 
     return render_template('login.html')
-
 
 @app.route("/register", methods=['GET','POST'])
 def register():
@@ -29,8 +29,6 @@ def register():
         position = request.form.get('Position')
         registerNewUser(email)
 
-
-
     return render_template('register.html')
 
 @app.route("/forgotpassword")
@@ -41,6 +39,42 @@ def forgotpassword():
 def test():
     dict={"data":{"test":"Bayside"}}
     return json.dumps(dict)
+
+
+@app.route("/home")
+def home():
+    title = 'HOME'
+    return render_template("home.html",title=title)
+
+@app.route("/dashboard")
+def dashboard():
+    title = 'DASHBOARD'
+    return render_template("dashboard.html",title=title)
+
+@app.route("/pinned")
+def pinned():
+    title = 'PINNED'
+    return render_template("pinned.html",title=title)
+
+@app.route("/shared")
+def shared():
+    title = 'PINNED'
+    return render_template("shared.html",title=title)
+
+@app.route("/deleted")
+def deleted():
+    title = 'DELETE'
+    return render_template("delete.html",title=title)
+
+@app.route("/settings")
+def settings():
+    title = 'SETTINGS'
+    return render_template("settings.html",title=title)
+
+@app.route("/admin")
+def admin():
+    title = 'ADMIN'
+    return render_template("admin.html",title=title)
 
 
 if __name__ == "__main__":
