be19849ad3a271e64ade00e4fd9b083c2bff27e8
Paul Ian <paulian829@gmail.com>
2021-11-03 09:47:29 +0800

    Add flask

--- flask_app.py
+++ flask_app.py
@@ -1,72 +1,46 @@
 from enum import unique
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, ValidationError
-from flask_bcrypt import Bcrypt
-
+from flask import Flask, render_template,request,json
+from database import registerNewUser
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SECRET_KEY'] = 'thisisseacretkey'
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
-    name = db.Column(db.String(20), nullable=False)
-    position = db.Column(db.String(50), nullable=False)
-    date_created = db.column(db.DateTime)
-
-
-class LoginForm(FlaskForm):
-    email = StringField(validators=[InputRequired(), Length(
-        min=4, max=20)], render_kw={"placeholder":"email"})
-    
-    password = PasswordField(validators=[InputRequired(), Length(
-        min=4, max=20)], render_kw={"placeholder":"password"})
-
-    submit = SubmitField("Login")
-
-class RegisterForm(FlaskForm):
-    email = StringField(validators=[InputRequired(), Length(
-        min=4, max=20)], render_kw={"placeholder":"email"})
-    
-    password = PasswordField(validators=[InputRequired(), Length(
-        min=4, max=20)], render_kw={"placeholder":"password"})
-
-    rePassword = PasswordField(validators=[InputRequired(), Length(
-        min=4, max=20)], render_kw={"placeholder":"repeat password"})
-
-    name = StringField(validators=[InputRequired(), Length(
-        min=4, max=50)], render_kw={"placeholder":"name"})
-
-    position = StringField(validators=[InputRequired(), Length(
-        min=4, max=50)], render_kw={"placeholder":"position"}) 
-
-    submit = SubmitField("Login")
 
 @app.route("/",methods=['GET','POST'])
 def login():
-    form = LoginForm()
+    if request.method == 'POST':
+        email = request.form.get('Email')
+        password = request.form.get('Password')
+        print(email)
+        print(password)
 
 
-    return render_template('login.html',form=form)
+    return render_template('login.html')
 
 
-@app.route("/register")
+@app.route("/register", methods=['GET','POST'])
 def register():
 
-    form = RegisterForm()
+    if request.method == 'POST':
+        email = request.form.get('Email')
+        password = request.form.get('Password')
+        repeatPass = request.form.get('RepeatPass')
+        name = request.form.get('Name')
+        phoneNumber = request.form.get('PhoneNumber')
+        position = request.form.get('Position')
+        registerNewUser(email)
 
-    return render_template('register.html',form = form)
+
+
+    return render_template('register.html')
 
 @app.route("/forgotpassword")
 def forgotpassword():
     return render_template('forgot-password.html')
+
+@app.route("/test")
+def test():
+    dict={"data":{"test":"Bayside"}}
+    return json.dumps(dict)
 
 
 if __name__ == "__main__":
