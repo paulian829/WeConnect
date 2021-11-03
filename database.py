a4f9cd3a1e02e913ffb8b93551e93ef36c60c14b
Paul Ian <paulian829@gmail.com>
2021-11-04 00:59:49 +0800

    DAtabase

--- database.py
+++ database.py
@@ -1,29 +1,29 @@
-# import mysql.connector
+import mysql.connector
 
-# mydb = mysql.connector.connect(
-#   host="localhost",
-#   user="root",
-#   password="",
-#   database='weconnect'
-# )
+mydb = mysql.connector.connect(
+  host="paulian829.mysql.pythonanywhere-services.com",
+  user="paulian829",
+  password="Shokugeki2021!",
+  database='paulian829$weconnect'
+)
 
 
-# def registerNewUser(email):
-#     print(email)
-#     mycursor = mydb.cursor()
-#     sql = "INSERT INTO users (Email, Password, Name, PhoneNumber, Position) VALUES ('%s')"
-#     val = ("'John'")
-#     mycursor.execute(sql % (email))
-#     mydb.commit()
-#     print(mycursor.rowcount, "record inserted.")
+def registerNewUser(email):
+    print(email)
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO users (Email, Password, Name, PhoneNumber, Position) VALUES ('%s')"
+    val = ("'John'")
+    mycursor.execute(sql % (email))
+    mydb.commit()
+    print(mycursor.rowcount, "record inserted.")
 
-# def newPosition(position):
-#     mycursor = mydb.cursor()
-#     sql = "INSERT INTO position (position_name) VALUES ('%s')"
-#     val = (position)
-#     print(sql % (val))
-#     mycursor.execute(sql % (val))
-#     mydb.commit()
-#     print(mycursor.rowcount, "record inserted.")
+def newPosition(position):
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO position (position_name) VALUES ('%s')"
+    val = (position)
+    print(sql % (val))
+    mycursor.execute(sql % (val))
+    mydb.commit()
+    print(mycursor.rowcount, "record inserted.")
 
-#newPosition('Principal')
+newPosition('Principal')
