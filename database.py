b2171a6f2edfb848abcc427db583daf96cc2a8d6
Paul Ian <paulian829@gmail.com>
2021-11-10 22:26:03 +0800

    Updates

--- database.py
+++ database.py
@@ -9,23 +9,83 @@ import hashlib
 #   database='paulian829$weconnect'
 # )
 
+
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   password="",
   database='weconnect'
 )
-
-
-
-def registerNewUser(email):
-  print(email)
+def getAll(table):
   mycursor = mydb.cursor()
-  sql = "INSERT INTO users (Email, Password, Name, PhoneNumber, Position) VALUES ('%s')"
-  val = ("'John'")
-  mycursor.execute(sql % (email))
-  mydb.commit()
-  print(mycursor.rowcount, "record inserted.")
+  sql = "SELECT * FROM position"
+  mycursor.execute(sql)
+  myresult = mycursor.fetchall()
+  print(myresult)
+  return myresult
+
+
+def registerNewUser(username,email,  password, repeatPass, name, phoneNumber, position):
+  #Check if Password are the same
+  if(password != repeatPass):
+    return("Password not The Same")
+  #Hashing
+  password = bytes(password, encoding= 'utf-8')
+  hash_object = hashlib.sha1(str(password).encode('utf-8'))
+  hex_dig = hash_object.hexdigest()
+  try:
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO users (Username,Email, Password, Name, PhoneNumber, Position) VALUES ('%s','%s','%s','%s','%s','%s')"
+    val = (username, email, hex_dig, name, phoneNumber, position)
+    print(val)
+    mycursor.execute(sql % (val))
+    print(mycursor.rowcount, "Records Added")
+    mydb.commit()
+    return "Success"
+  except Exception as e:
+    if "Duplicate entry" in str(e):
+      return ("Duplicate error")
+
+
+def loginDB(username, password):
+  #check if Username Exists
+  mycursor = mydb.cursor()
+  sql = "SELECT * FROM users WHERE username = '%s'"
+  val = (username)
+  mycursor.execute(sql % (val))
+  myresult = mycursor.fetchall()
+  if(len(myresult)== 0):
+    return "Error"
+  realPassword = myresult[0][3]
+  #Compare Password
+  typedpassword = bytes(password, encoding= 'utf-8')
+  hash_object = hashlib.sha1(str(typedpassword).encode('utf-8'))
+  hex_dig = hash_object.hexdigest()
+
+  if(realPassword == hex_dig):
+    print(myresult)
+    return myresult
+  
+  return "Error"
+
+
+def getUserData(id):
+  mycursor = mydb.cursor()
+  sql = "SELECT * FROM users WHERE id = '%s'"
+  val = (id)
+  mycursor.execute(sql % (val))
+  myresult = mycursor.fetchall()
+  return myresult
+
+def getPosition(positionID):
+  mycursor = mydb.cursor()
+  sql = "SELECT * FROM position WHERE id ='%s'"
+  val = (positionID)
+  print(sql % (val))
+  mycursor.execute(sql % (val))
+  myresult = mycursor.fetchone()
+  return myresult[1]
+
 
 def newPosition(position):
   mycursor = mydb.cursor()
@@ -38,7 +98,7 @@ def newPosition(position):
 
 
 def hashtest(password, password2):
-  hash_object = hashlib.sha1(password)
+  hash_object = hashlib.sha256(password)
   hex_dig = hash_object.hexdigest()
   print(hex_dig)
 
@@ -47,9 +107,3 @@ def hashtest(password, password2):
   
   print (ahex == hex_dig)
   
-
-
-
-vartest = b'password'
-varb = b'password'
-hashtest(vartest,varb)
--- flask_app.py
+++ flask_app.py
@@ -1,35 +1,48 @@
 from enum import unique
-from flask import Flask, render_template,request,json,redirect,url_for
-#from database import registerNewUser
+from flask import Flask, render_template,request,json,redirect,url_for,session
+from database import *
 
 app = Flask(__name__)
 
 
 @app.route("/",methods=['GET','POST'])
 def login():
+
+    session['logged_in'] = False
     if request.method == 'POST':
-        email = request.form.get('Email')
+        username = request.form.get('Username')
         password = request.form.get('Password')
-        print(email)
-        print(password)
+        if (len(username) == 0 or len(password) == 0 ):
+            result = "Error"
+            return render_template('login.html',result = result)
+        result = loginDB(username, password)
+        if(result == "Error"):
+            return render_template('login.html',result = result)
+
+        session['logged_in'] = True
+        session['userID'] = result[0][0]
+        session['name'] = result[0][4]
         return redirect(url_for('home'))
-
-
     return render_template('login.html')
+
 
 @app.route("/register", methods=['GET','POST'])
 def register():
-
+    positions = getAll("position")
     if request.method == 'POST':
+        username = request.form.get("Username")
         email = request.form.get('Email')
         password = request.form.get('Password')
         repeatPass = request.form.get('RepeatPass')
         name = request.form.get('Name')
         phoneNumber = request.form.get('PhoneNumber')
         position = request.form.get('Position')
-        #registerNewUser(email)
+        result = registerNewUser(username, email, password, repeatPass, name, phoneNumber, position)
+        print(result)
+        return render_template('register.html',result = result, positions = positions)
+    else:
+        return render_template('register.html',positions = positions)
 
-    return render_template('register.html')
 
 @app.route("/forgotpassword")
 def forgotpassword():
@@ -44,7 +57,26 @@ def test():
 @app.route("/home")
 def home():
     title = 'HOME'
-    return render_template("home.html",title=title)
+    items = [
+            { "message": 'Foo.pdf', "key": 0 },
+            { "message": 'Foo.pdf', "key": 1 },
+            { "message": 'Foo.pdf', "key": 2 },
+            { "message": 'Foo.pdf', "key": 3 },
+            { "message": 'Foo.pdf', "key": 4 },
+            { "message": 'Bar.pdf', "key": 5 },
+            { "message": 'Bar.pdf', "key": 6 },
+            { "message": 'Bar.pdf', "key": 7 },
+            { "message": 'Bar.pdf', "key": 8 },
+            { "message": 'Bar.pdf', "key": 9 },
+        ]
+    # print(session['userID'])
+    # print(session['name'])
+    print(session)
+    if session['logged_in'] is False:
+        return redirect(url_for('login'))
+
+    return render_template("home.html",title=title, name=session['name'])
+
 
 @app.route("/dashboard")
 def dashboard():
@@ -55,7 +87,6 @@ def dashboard():
 def pinned():
     title = 'PINNED'
     return render_template("pinned.html",title=title)
-
 @app.route("/shared")
 def shared():
     title = 'PINNED'
@@ -79,10 +110,29 @@ def admin():
 @app.route("/profile/")
 def profile():
     title = 'PROFILE'
-    return render_template("profile.html",title=title)
+    #Check if Sessions Exist
+    if session['logged_in'] is False:
+        return redirect(url_for('login'))
+
+    result = getUserData(session['userID'])
+    position = getPosition(result[0][6])
+    positions = getAll("position")
+    return render_template("profile.html",title=title, profile_details = result, position=position, positions = positions)
+
+    
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html")
 
 
 if __name__ == "__main__":
+    app.secret_key = 'super secret key'
     app.run(debug=True, use_reloader=True)
 
 
