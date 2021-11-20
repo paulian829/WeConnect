457c789ee6e1635a3f50a148180545bfa1806796
Paul <paulian829@gmail.com>
2021-11-20 13:41:17 +0800

    ASDAsd

--- database.py
+++ database.py
@@ -11,140 +11,143 @@ import hashlib
 
 
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="",
-  database='weconnect'
+    host="localhost",
+    user="root",
+    password="",
+    database='weconnect'
 )
+
+
 def getAll(table):
-  mycursor = mydb.cursor()
-  sql = "SELECT * FROM position"
-  mycursor.execute(sql)
-  myresult = mycursor.fetchall()
-  print(myresult)
-  return myresult
+    mycursor = mydb.cursor()
+    mycursor.callproc("SelectAllPostions")
+    for result in mycursor.stored_results():
+        return (result.fetchall())
 
 
 def registerNewUser(email,  password, repeatPass, firstname, lastname, phoneNumber, position):
-  #Check if Password are the same
-  if(password != repeatPass):
-    return("Password not The Same")
-  #Hashing
-  password = bytes(password, encoding= 'utf-8')
-  hash_object = hashlib.sha256(str(password).encode('utf-8'))
-  hex_dig = hash_object.hexdigest()
-  try:
-    mycursor = mydb.cursor()
-    sql = "INSERT INTO users (Email, Password, FirstName, LastName, PhoneNumber, Position) VALUES ('%s','%s','%s','%s','%s','%s')"
-    val = (email, hex_dig, firstname, lastname, phoneNumber, position)
-    print(val)
-    mycursor.execute(sql % (val))
-    print(mycursor.rowcount, "Records Added")
-    mydb.commit()
-    return "Success"
-  except Exception as e:
-    if "Duplicate entry" in str(e):
-      return ("Duplicate error")
+    # Check if Password are the same
+    if(password != repeatPass):
+        return("Password not The Same")
+    # Hashing
+    password = bytes(password, encoding='utf-8')
+    hash_object = hashlib.sha256(str(password).encode('utf-8'))
+    hex_dig = hash_object.hexdigest()
+    try:
+        mycursor = mydb.cursor()
+        args = (email, hex_dig, firstname, lastname, phoneNumber, position, )
+        mycursor.callproc('addUser', args)
+        mydb.commit()
+        return "Success"
+    except Exception as e:
+        if "Duplicate entry" in str(e):
+            return ("Duplicate error")
+
 
 def updateUserDB(id, email, firstName, lastName, phoneNumber, position):
-  mycursor = mydb.cursor()
-  sql = "UPDATE users SET Email = '%s', FirstName = '%s', LastName='%s', PhoneNumber='%s', Position = '%s' WHERE id = %s"
-  val = (email, firstName , lastName, phoneNumber, position, id )
-  mycursor.execute(sql % (val))
-  mydb.commit()
-  if mycursor.rowcount > 0:
-    return True
-  return False
+    mycursor = mydb.cursor()
+    val = (email, firstName, lastName, phoneNumber, position, id, )
+    mycursor.callproc("updateUser", val)
+    mydb.commit()
+    if mycursor.rowcount > 0:
+        return True
+    return False
+
 
 def deleteUserDB(id):
-  print(id)
-  mycursor = mydb.cursor()
-  mycursor.callproc("")
-  sql = "DELETE FROM users WHERE id = %s"
-  val = (id)
-  mycursor.execute(sql % (val))
-  mydb.commit()
-  return "Delete" 
+    print(id)
+    mycursor = mydb.cursor()
+    mycursor.callproc("deleteUser", (id, ))
+    mydb.commit()
+    return "Delete"
+
 
 def loginDB(email, password):
-  #check if Username Exists
-  mycursor = mydb.cursor()
-  sql = "SELECT * FROM users WHERE Email = '%s'"
-  val = (email)
-  mycursor.execute(sql % (val))
-  myresult = mycursor.fetchall()
-  print(len(myresult))
-  if(len(myresult)== 0):
-    
+    # check if Username Exists
+    mycursor = mydb.cursor()
+    val = (email,)
+    mycursor.callproc("SelectUserEmail", val)
+    for result in mycursor.stored_results():
+        myresult = (result.fetchall())
+    if(len(myresult) == 0):
+
+        return "Error"
+    realPassword = myresult[0][2]
+    # Compare Password
+    typedpassword = bytes(password, encoding='utf-8')
+    hash_object = hashlib.sha256(str(typedpassword).encode('utf-8'))
+    hex_dig = hash_object.hexdigest()
+    print(realPassword, hex_dig)
+    if(realPassword == hex_dig):
+        print(myresult)
+        return myresult
+
+    print("Not Equal")
     return "Error"
-  realPassword = myresult[0][2]
-  #Compare Password
-  typedpassword = bytes(password, encoding= 'utf-8')
-  hash_object = hashlib.sha256(str(typedpassword).encode('utf-8'))
-  hex_dig = hash_object.hexdigest()
-  print(realPassword, hex_dig)
-  if(realPassword == hex_dig):
-    print(myresult)
-    return myresult
-  
-  print("Not Equal")
-  return "Error"
 
 
 def getUserData(id):
-  mycursor = mydb.cursor()
-  sql = "SELECT * FROM users WHERE id = '%s'"
-  val = (id)
-  mycursor.execute(sql % (val))
-  myresult = mycursor.fetchall()
-  return myresult
+    mycursor = mydb.cursor()
+    val = (id,)
+    mycursor.callproc("selectUserID", val)
+    for result in mycursor.stored_results():
+        return (result.fetchall())
+
 
 def getAllUsers():
-  mycursor = mydb.cursor()
-  sql = "SELECT * FROM users "
-  mycursor.execute(sql)
-  myresult = mycursor.fetchall()
-  return myresult
+    mycursor = mydb.cursor()
+    mycursor.callproc("SelectAllUsers")
+    for result in mycursor.stored_results():
+        return (result.fetchall())
 
 
 def getPosition(positionID):
-  mycursor = mydb.cursor()
-  sql = "SELECT * FROM position WHERE id ='%s'"
-  val = (positionID)
-  print(sql % (val))
-  mycursor.execute(sql % (val))
-  myresult = mycursor.fetchone()
-  return myresult[1]
+    mycursor = mydb.cursor()
+    val = (positionID,)
+    mycursor.callproc("selectPositionID", val)
+    for result in mycursor.stored_results():
+        return (result.fetchall())
 
 
 def newPosition(position):
-  mycursor = mydb.cursor()
-  sql = "INSERT INTO position (position_name) VALUES ('%s')"
-  val = (position)
-  print(sql % (val))
-  mycursor.execute(sql % (val))
-  mydb.commit()
-  print(mycursor.rowcount, "record inserted.")
+    try:
+      mycursor = mydb.cursor()
+      val = (position,)
+      mycursor.callproc('addPosition', val)
+      mydb.commit()
+      return True
+    except:
+      return False
 
+    
 
+    
+
+# ==========================================
+# TESTING FUNCTIONS
 def hashtest(password, password2):
-  hash_object = hashlib.sha256(password)
-  hex_dig = hash_object.hexdigest()
+    hash_object = hashlib.sha256(password)
+    hex_dig = hash_object.hexdigest()
 
-  a = hashlib.sha1(password2)
-  ahex = a.hexdigest()
-  
-  print (ahex == hex_dig)
+    a = hashlib.sha1(password2)
+    ahex = a.hexdigest()
 
-hashtest(b'pass1',b'pass1')
-  
+    print(ahex == hex_dig)
+
+
+hashtest(b'pass1', b'pass1')
+
 
 def getusers():
-  mycursor = mydb.cursor()
-  mycursor.callproc("SelectAllUsers")
-  for result in mycursor.stored_results():
-    print(result.fetchall())
+    mycursor = mydb.cursor()
+    mycursor.callproc("selectUser", ['admin@gmail.com', ])
+    for result in mycursor.stored_results():
+        result = (result.fetchall())
+
+    print(result)
 
 
-
-getusers()
+# print(registerNewUser('test3','test1','test1','test1','test','test',1))
+# print(getusers())
+# print(deleteUserDB(85))
+# print(newPosition('test'))
