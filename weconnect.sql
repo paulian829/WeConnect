49aa813b403f202e3bc04d2e4fab1ea54715414a
Paul <paulian829@gmail.com>
2021-12-10 09:23:56 +0800

    update sql

--- weconnect.sql
+++ weconnect.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2021 at 12:51 PM
+-- Generation Time: Dec 10, 2021 at 02:20 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.3.31
 
@@ -25,78 +25,91 @@ DELIMITER $$
 --
 -- Procedures
 --
-CREATE DEFINER=`root`@`localhost` PROCEDURE `addPosition` (IN `test` VARCHAR(50))  BEGIN
+CREATE  PROCEDURE `addPosition` (IN `test` VARCHAR(50))  BEGIN
 	INSERT INTO position (position_name) VALUES (test);
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `addUser` (IN `uEmail` VARCHAR(50), IN `uPassword` VARCHAR(100), IN `uFirstName` VARCHAR(50), IN `uLastName` VARCHAR(50), IN `uPhoneNumber` VARCHAR(11), IN `uPosition` INT(1))  BEGIN
+CREATE  PROCEDURE `addUser` (IN `uEmail` VARCHAR(50), IN `uPassword` VARCHAR(100), IN `uFirstName` VARCHAR(50), IN `uLastName` VARCHAR(50), IN `uPhoneNumber` VARCHAR(11), IN `uPosition` INT(1))  BEGIN
 INSERT INTO users (Email, Pass, FirstName, LastName, PhoneNumber, Position) VALUES (uEmail, uPassword, uFirstName, uLastName, uPhoneNumber, uPosition);
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `deleteUser` (IN `uid` INT(11))  BEGIN
+CREATE  PROCEDURE `deleteUser` (IN `uid` INT(11))  BEGIN
 DELETE FROM users WHERE id = uid;
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `getAllFiles` (IN `uid` INT)  BEGIN
-SELECT * FROM files where UploadedByID = uid;
+CREATE  PROCEDURE `emptyTrash` (IN `uid` INT)  BEGIN
+DELETE FROM files WHERE status = 4 AND UploadedByID = uid;
 
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `getFile` (IN `uid` INT(11))  BEGIN
+CREATE  PROCEDURE `getAllFiles` (IN `uid` INT)  BEGIN
+SELECT * FROM files where UploadedByID = uid ORDER BY FileID DESC;
+
+END$$
+
+CREATE  PROCEDURE `getFile` (IN `uid` INT(11))  BEGIN
 	SELECT * FROM files WHERE FileID = uid;
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `getNFiles` (IN `uid` INT, IN `number` INT)  BEGIN
-SELECT * FROM files WHERE UploadedByID = uid ORDER BY FileID DESC LIMIT number;
+CREATE  PROCEDURE `getNFiles` (IN `uid` INT, IN `number` INT)  BEGIN
+SELECT * FROM files WHERE UploadedByID = uid AND status=0 ORDER BY FileID DESC LIMIT number;
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `getNumberOfFilesPassed` (IN `uid` INT)  BEGIN
+CREATE  PROCEDURE `getNumberOfFilesPassed` (IN `uid` INT)  BEGIN
 	SELECT * FROM files WHERE UploadedByID = uid AND status=1;
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `getNumberOfFilesUploaded` (IN `uid` INT)  BEGIN
+CREATE  PROCEDURE `getNumberOfFilesUploaded` (IN `uid` INT)  BEGIN
 	SELECT * FROM files WHERE UploadedByID = uid AND status=0;
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `number_of_files_deadline` (IN `uid` INT)  BEGIN
+CREATE  PROCEDURE `moveFileToThrash` (IN `id` INT)  BEGIN
+UPDATE files SET status = 4 where FileID = id;
+END$$
+
+CREATE  PROCEDURE `number_of_files_deadline` (IN `uid` INT)  BEGIN
 	SELECT * FROM files WHERE UploadedByID = uid AND status=2;
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `number_of_files_nearing_Deadline` (IN `uid` INT)  BEGIN
+CREATE  PROCEDURE `number_of_files_nearing_Deadline` (IN `uid` INT)  BEGIN
 	SELECT * FROM files WHERE UploadedByID = uid AND status=3;
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `saveFile` (IN `uFileName` VARCHAR(100), IN `uFileType` VARCHAR(20), IN `uFileSize` INT(11), IN `uFileContentType` VARCHAR(100), IN `uUploadedByID` INT(11), IN `uShare_to_user` INT(11), IN `uShare_to_group` INT(11), IN `uDeadLine` DATE, IN `uRevision` INT(11))  BEGIN
-INSERT INTO files (FileName, FileType,FileSize, FileContentType, UploadedByID, Share_to_user, Share_to_group, DeadLine, Revision) VALUES (uFileName, uFileType, uFileSize, uFileContentType, uUploadedByID, uShare_to_user, uShare_to_group, uDeadLine, uRevision);
+CREATE  PROCEDURE `Restore` (IN `uID` INT)  BEGIN
+UPDATE files SET status = 0 WHERE FileID = uID;
+END$$
+
+CREATE  PROCEDURE `saveFile` (IN `uFileName` VARCHAR(100), IN `uFileType` VARCHAR(20), IN `uFileSize` INT(11), IN `uFileContentType` VARCHAR(100), IN `uUploadedByID` INT(11), IN `uShare_to_user` INT(11), IN `uShare_to_group` INT(11), IN `uDeadLine` DATE, IN `uRevision` INT(11), IN `date` DATE)  BEGIN
+INSERT INTO files (FileName, FileType,FileSize, FileContentType, UploadedByID, Share_to_user, Share_to_group, DeadLine, Revision,dateUploaded) VALUES (uFileName, uFileType, uFileSize, uFileContentType, uUploadedByID, uShare_to_user, uShare_to_group, uDeadLine, uRevision,date);
 SELECT last_insert_id();
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `SelectAllPostions` ()  BEGIN
+CREATE  PROCEDURE `SelectAllPostions` ()  BEGIN
 	SELECT * FROM position;
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `SelectAllUsers` ()  BEGIN
+CREATE  PROCEDURE `SelectAllUsers` ()  BEGIN
 	SELECT * FROM users;
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `selectPositionID` (IN `uID` INT(11))  BEGIN
+CREATE  PROCEDURE `selectPositionID` (IN `uID` INT(11))  BEGIN
 	SELECT * FROM position WHERE id = uID;
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `selectUserEmail` (IN `uEmail` VARCHAR(50))  BEGIN
+CREATE  PROCEDURE `selectUserEmail` (IN `uEmail` VARCHAR(50))  BEGIN
 	SELECT * FROM users WHERE email = uEmail;
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `selectUserID` (IN `uID` INT(11))  BEGIN
+CREATE  PROCEDURE `selectUserID` (IN `uID` INT(11))  BEGIN
 	SELECT * FROM users WHERE id = uID;
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `updateDOC` (IN `json` LONGTEXT, IN `uID` INT)  BEGIN
+CREATE  PROCEDURE `updateDOC` (IN `json` LONGTEXT, IN `uID` INT)  BEGIN
 UPDATE files SET block_doc_json = json WHERE FileID = uID;
 
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `updateUser` (IN `uEmail` VARCHAR(50), IN `uFirstname` VARCHAR(50), IN `uLastName` VARCHAR(50), IN `uPhoneNumber` VARCHAR(11), IN `uPosition` INT(11), IN `uID` INT(11))  BEGIN
+CREATE  PROCEDURE `updateUser` (IN `uEmail` VARCHAR(50), IN `uFirstname` VARCHAR(50), IN `uLastName` VARCHAR(50), IN `uPhoneNumber` VARCHAR(11), IN `uPosition` INT(11), IN `uID` INT(11))  BEGIN
 UPDATE users SET Email = uEmail, FirstName = uFirstname, LastName= uLastName, PhoneNumber=uPhoneNumber, Position = uPosition WHERE id = uID;
 
 END$$
@@ -120,7 +133,7 @@ CREATE TABLE `files` (
   `Share_to_group` int(11) NOT NULL,
   `DeadLine` date NOT NULL,
   `Revision` int(11) NOT NULL,
-  `DateUploaded` date NOT NULL DEFAULT current_timestamp(),
+  `DateUploaded` date NOT NULL,
   `block_doc_json` longtext NOT NULL DEFAULT '[{"id":"xPmqz3gBHy","type":"header","data":{"text":"This is the Heading!","level":1}}]',
   `status` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -130,15 +143,8 @@ CREATE TABLE `files` (
 --
 
 INSERT INTO `files` (`FileID`, `FileName`, `FileType`, `FileSize`, `FileContentType`, `UploadedByID`, `Share_to_user`, `Share_to_group`, `DeadLine`, `Revision`, `DateUploaded`, `block_doc_json`, `status`) VALUES
-(25, 'FILENAME (10).png', 'raw file', 344296, 'image/png', 82, 0, 1, '1231-03-12', 1, '2021-11-27', '', 0),
-(26, 'FILENAME (10).png', 'raw file', 1, 'image/png', 82, 0, 1, '1231-03-12', 1, '2021-11-27', '', 0),
-(27, 'testFILENAME (10).png', 'raw file', 1, 'image/png', 82, 0, 1, '1231-03-12', 1, '2021-11-27', '', 3),
-(28, 'testFILENAME (9).png', 'raw file', 1, 'image/png', 82, 0, 1, '1231-03-12', 1, '2021-11-27', '', 1),
-(29, '82FILENAME (8).png', 'raw file', 1, 'image/png', 82, 0, 1, '1231-03-12', 1, '2021-11-27', '', 2),
-(31, 'test', 'block doc', 1, 'test', 83, 0, 1, '1231-03-12', 1, '2021-11-27', '[{\"id\":\"xPmqz3gBHy\",\"type\":\"header\",\"data\":{\"text\":\"This is the Heading!\",\"level\":1}},{\"id\":\"XNKBJiLbTV\",\"type\":\"header\",\"data\":{\"text\":\"1234567890\",\"level\":2}}]', 0),
-(45, 'block test document', 'block doc', 1, 'test', 82, 0, 1, '0000-00-00', 1, '2021-11-27', '[{\"id\":\"xPmqz3gBHy\",\"type\":\"header\",\"data\":{\"text\":\"This is the Heading!\",\"level\":1}},{\"id\":\"7Y6tKs3tg_\",\"type\":\"paragraph\",\"data\":{\"text\":\"This is the Heading!\"}},{\"id\":\"JhKge7cOS8\",\"type\":\"list\",\"data\":{\"style\":\"ordered\",\"items\":[\"123123\",\"1123123123\",\"asdas\",\"dadada\",\"asdasd\"]}},{\"id\":\"T70xqF7P6j\",\"type\":\"paragraph\",\"data\":{\"text\":\"das\"}},{\"id\":\"rcjrJ1YvAi\",\"type\":\"header\",\"data\":{\"text\":\"asdasd\",\"level\":1}}]', 0),
-(46, '82Annual budget.xlsx', 'raw file', 1, 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 82, 0, 1, '1231-03-12', 1, '2021-11-28', '[{\"id\":\"xPmqz3gBHy\",\"type\":\"header\",\"data\":{\"text\":\"This is the Heading!\",\"level\":1}}]', 0),
-(47, '82FILENAME (1).png', 'raw file', 1, 'image/png', 82, 0, 1, '1221-03-12', 1, '2021-11-28', '[{\"id\":\"xPmqz3gBHy\",\"type\":\"header\",\"data\":{\"text\":\"This is the Heading!\",\"level\":1}}]', 0);
+(31, 'test', 'block doc', 1, 'test', 83, 0, 1, '1231-03-12', 1, '2021-11-27', '[{\"id\":\"xPmqz3gBHy\",\"type\":\"header\",\"data\":{\"text\":\"This is the Heading!\",\"level\":1}},{\"id\":\"XNKBJiLbTV\",\"type\":\"header\",\"data\":{\"text\":\"1234567890\",\"level\":2}}]', 4),
+(57, 'CAMILLE-P.-MERENE.pdf', 'raw file', 1, 'application/pdf', 82, 0, 1, '1111-11-11', 1, '2021-12-10', '[{\"id\":\"xPmqz3gBHy\",\"type\":\"header\",\"data\":{\"text\":\"This is the Heading!\",\"level\":1}}]', 0);
 
 -- --------------------------------------------------------
 
@@ -220,7 +226,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `files`
 --
 ALTER TABLE `files`
-  MODIFY `FileID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `FileID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 
 --
 -- AUTO_INCREMENT for table `position`
