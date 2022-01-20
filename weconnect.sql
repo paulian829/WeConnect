a1ff47db8f16587cd23aa643a14ca5d83ad93a44
Paul <paulian829@gmail.com>
2022-01-20 08:07:27 +0800

    updates

--- weconnect.sql
+++ weconnect.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 12, 2022 at 12:00 PM
+-- Generation Time: Jan 20, 2022 at 01:06 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.3.31
 
@@ -25,178 +25,182 @@ DELIMITER $$
 --
 -- Procedures
 --
-CREATE  PROCEDURE `addPosition` (IN `test` VARCHAR(50))  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addPosition` (IN `test` VARCHAR(50))  BEGIN
 	INSERT INTO position (position_name) VALUES (test);
 END$$
 
-CREATE  PROCEDURE `addProfilePicDB` (IN `str` VARCHAR(200), IN `uID` INT(11))  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addProfilePicDB` (IN `str` VARCHAR(200), IN `uID` INT(11))  BEGIN
 UPDATE users SET profilePic = str WHERE id = uID;
 END$$
 
-CREATE  PROCEDURE `AddTask` (IN `Name` VARCHAR(100), IN `createdBy` INT(11), IN `dateCreated` DATETIME, IN `deadline` DATETIME, IN `description` TEXT, IN `Tstatus` VARCHAR(100), IN `scheduled` VARCHAR(50))  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `AddTask` (IN `Name` VARCHAR(100), IN `createdBy` INT(11), IN `dateCreated` DATETIME, IN `deadline` DATETIME, IN `description` TEXT, IN `Tstatus` VARCHAR(100), IN `scheduled` VARCHAR(50))  BEGIN
 INSERT INTO tasks (TaskName, TaskCreatedBy, TaskDateCreated, TaskDeadline, TaskDescription,TaskStatus, TaskSchedule) VALUES (Name, createdBy,dateCreated, deadline, description, Tstatus, scheduled);
 SELECT last_insert_id();
 END$$
 
-CREATE  PROCEDURE `addUser` (IN `uEmail` VARCHAR(50), IN `uPassword` VARCHAR(100), IN `uFirstName` VARCHAR(50), IN `uLastName` VARCHAR(50), IN `uPhoneNumber` VARCHAR(11), IN `uPosition` INT(1))  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addUser` (IN `uEmail` VARCHAR(50), IN `uPassword` VARCHAR(100), IN `uFirstName` VARCHAR(50), IN `uLastName` VARCHAR(50), IN `uPhoneNumber` VARCHAR(11), IN `uPosition` INT(1))  BEGIN
 INSERT INTO users (Email, Pass, FirstName, LastName, PhoneNumber, Position) VALUES (uEmail, uPassword, uFirstName, uLastName, uPhoneNumber, uPosition);
 END$$
 
-CREATE  PROCEDURE `checkUserUpload` (IN `userID` INT, IN `TtaskID` INT)  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `checkUserUpload` (IN `userID` INT, IN `TtaskID` INT)  BEGIN
 	SELECT * FROM files WHERE (UploadedByID = userID AND taskID = TtaskID);
 END$$
 
-CREATE  PROCEDURE `deleteCommentDB` (IN `uid` INT)  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `deleteCommentDB` (IN `uid` INT)  BEGIN
 DELETE FROM comments WHERE ID = uid;
 END$$
 
-CREATE  PROCEDURE `deleteFile` (IN `uid` INT)  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `deleteFile` (IN `uid` INT)  BEGIN
 DELETE FROM files WHERE FileID = uid;
 
 END$$
 
-CREATE  PROCEDURE `deleteTaskDb` (IN `uid` INT(11))  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `deleteTaskDb` (IN `uid` INT(11))  BEGIN
 DELETE FROM tasks WHERE TaskID = uid;
 END$$
 
-CREATE  PROCEDURE `deleteUser` (IN `uid` INT(11))  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `deleteUser` (IN `uid` INT(11))  BEGIN
 DELETE FROM users WHERE id = uid;
 END$$
 
-CREATE  PROCEDURE `emptyTrash` (IN `uid` INT)  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `emptyTrash` (IN `uid` INT)  BEGIN
 DELETE FROM files WHERE status = 4 AND UploadedByID = uid;
 
 END$$
 
-CREATE  PROCEDURE `getAllFiles` (IN `uid` INT)  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getAllComments` ()  BEGIN
+	SELECT * FROM comments;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getAllFiles` (IN `uid` INT)  BEGIN
 SELECT * FROM files where UploadedByID = uid ORDER BY FileID DESC;
 
 END$$
 
-CREATE  PROCEDURE `getAllFilesForAdmin` ()  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getAllFilesForAdmin` ()  BEGIN
 SELECT * FROM files ORDER BY FileID DESC;
 END$$
 
-CREATE  PROCEDURE `getCommentsDB` (IN `uid` INT)  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getCommentsDB` (IN `uid` INT)  BEGIN
 	SELECT * FROM comments WHERE FileID = uid ORDER BY ID DESC;
 END$$
 
-CREATE  PROCEDURE `getFile` (IN `uid` INT(11))  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getFile` (IN `uid` INT(11))  BEGIN
 	SELECT * FROM files WHERE FileID = uid;
 END$$
 
-CREATE  PROCEDURE `getNFiles` (IN `uid` INT, IN `number` INT)  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getNFiles` (IN `uid` INT, IN `number` INT)  BEGIN
 SELECT * FROM files WHERE UploadedByID = uid AND status=0 ORDER BY FileID DESC LIMIT number;
 END$$
 
-CREATE  PROCEDURE `getNumberOfFilesPassed` (IN `uid` INT)  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getNumberOfFilesPassed` (IN `uid` INT)  BEGIN
 	SELECT * FROM files WHERE UploadedByID = uid AND status=1;
 END$$
 
-CREATE  PROCEDURE `getNumberOfFilesUploaded` (IN `uid` INT)  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getNumberOfFilesUploaded` (IN `uid` INT)  BEGIN
 	SELECT * FROM files WHERE UploadedByID = uid AND status=0;
 END$$
 
-CREATE  PROCEDURE `getTasksDB` (IN `sched` VARCHAR(100))  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getTasksDB` (IN `sched` VARCHAR(100))  BEGIN
 	SELECT * FROM tasks WHERE TaskSchedule = sched;
 END$$
 
-CREATE  PROCEDURE `getTeachers` ()  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getTeachers` ()  BEGIN
 	SELECT * FROM users WHERE Position = 4;
 END$$
 
-CREATE  PROCEDURE `getUserViaEmail` (IN `Uemail` VARCHAR(100))  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getUserViaEmail` (IN `Uemail` VARCHAR(100))  BEGIN
 	SELECT * FROM users WHERE Email = Uemail;
 END$$
 
-CREATE  PROCEDURE `moveFileToThrash` (IN `id` INT)  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `moveFileToThrash` (IN `id` INT)  BEGIN
 UPDATE files SET status = 4 where FileID = id;
 END$$
 
-CREATE  PROCEDURE `newComment` (IN `userID` INT, IN `fileID` INT, IN `TimeToday` INT(20), IN `Tcomment` VARCHAR(300))  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `newComment` (IN `userID` INT, IN `fileID` INT, IN `TimeToday` INT(20), IN `Tcomment` VARCHAR(300))  BEGIN
 	INSERT INTO comments (FileID,UserID,DateCreated,CommentMsg  ) VALUES (fileID, userID, TimeToday,Tcomment );
 END$$
 
-CREATE  PROCEDURE `number_of_files_deadline` (IN `uid` INT)  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `number_of_files_deadline` (IN `uid` INT)  BEGIN
 	SELECT * FROM files WHERE UploadedByID = uid AND status=2;
 END$$
 
-CREATE  PROCEDURE `number_of_files_nearing_Deadline` (IN `uid` INT)  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `number_of_files_nearing_Deadline` (IN `uid` INT)  BEGIN
 	SELECT * FROM files WHERE UploadedByID = uid AND status=3;
 END$$
 
-CREATE  PROCEDURE `resetPassword` (IN `NewPass` VARCHAR(100), IN `uID` INT)  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `resetPassword` (IN `NewPass` VARCHAR(100), IN `uID` INT)  BEGIN
 UPDATE users SET Pass = NewPass WHERE id = uID;
 
 END$$
 
-CREATE  PROCEDURE `Restore` (IN `uID` INT)  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Restore` (IN `uID` INT)  BEGIN
 UPDATE files SET status = 0 WHERE FileID = uID;
 END$$
 
-CREATE  PROCEDURE `saveFile` (IN `uFileName` VARCHAR(100), IN `uFileType` VARCHAR(20), IN `uFileSize` INT(11), IN `uFileContentType` VARCHAR(100), IN `uUploadedByID` INT(11), IN `uShare_to_user` INT(11), IN `uShare_to_group` INT(11), IN `uDeadLine` DATETIME, IN `uRevision` INT(11), IN `date` DATETIME, IN `tID` INT, IN `filepath` VARCHAR(100))  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `saveFile` (IN `uFileName` VARCHAR(100), IN `uFileType` VARCHAR(20), IN `uFileSize` INT(11), IN `uFileContentType` VARCHAR(100), IN `uUploadedByID` INT(11), IN `uShare_to_user` INT(11), IN `uShare_to_group` INT(11), IN `uDeadLine` DATETIME, IN `uRevision` INT(11), IN `date` DATETIME, IN `tID` INT, IN `filepath` VARCHAR(100))  BEGIN
 INSERT INTO files (FileName, FileType,FileSize, FileContentType, UploadedByID, Share_to_user, Share_to_group, DeadLine, Revision,dateUploaded,taskID,FilePathName) VALUES (uFileName, uFileType, uFileSize, uFileContentType, uUploadedByID, uShare_to_user, uShare_to_group, uDeadLine, uRevision,date,tID,filepath);
 SELECT last_insert_id();
 END$$
 
-CREATE  PROCEDURE `SelectAllPostions` ()  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SelectAllPostions` ()  BEGIN
 	SELECT * FROM position;
 END$$
 
-CREATE  PROCEDURE `SelectAllTask` ()  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SelectAllTask` ()  BEGIN
 	SELECT * FROM tasks;
 END$$
 
-CREATE  PROCEDURE `SelectAllUsers` ()  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SelectAllUsers` ()  BEGIN
 	SELECT * FROM users;
 END$$
 
-CREATE  PROCEDURE `SelectOneTask` (IN `uid` INT)  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SelectOneTask` (IN `uid` INT)  BEGIN
 	SELECT * FROM tasks WHERE TaskID = uid;
 END$$
 
-CREATE  PROCEDURE `selectPositionID` (IN `uID` INT(11))  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `selectPositionID` (IN `uID` INT(11))  BEGIN
 	SELECT * FROM position WHERE id = uID;
 END$$
 
-CREATE  PROCEDURE `selectUserEmail` (IN `uEmail` VARCHAR(50))  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `selectUserEmail` (IN `uEmail` VARCHAR(50))  BEGIN
 	SELECT * FROM users WHERE email = uEmail;
 END$$
 
-CREATE  PROCEDURE `selectUserID` (IN `uID` INT(11))  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `selectUserID` (IN `uID` INT(11))  BEGIN
 	SELECT * FROM users WHERE id = uID;
 END$$
 
-CREATE  PROCEDURE `SetPinned` (IN `uID` INT, IN `PinStatus` INT)  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SetPinned` (IN `uID` INT, IN `PinStatus` INT)  BEGIN
 UPDATE files SET pinned = PinStatus WHERE FileID = uID;
 END$$
 
-CREATE  PROCEDURE `Update Task` (IN `ID` INT, IN `name` VARCHAR(100), IN `imageBlob` BLOB, IN `deadline` DATETIME, IN `description` TEXT)  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Update Task` (IN `ID` INT, IN `name` VARCHAR(100), IN `imageBlob` BLOB, IN `deadline` DATETIME, IN `description` TEXT)  BEGIN
 UPDATE tasks SET TaskName = name, TaskImage = imageBlob, TaskDeadline = deadline, TaskDescription = description WHERE TaskID = ID;
 
 END$$
 
-CREATE  PROCEDURE `Update Task Status` (IN `ID` INT, IN `tstatus` VARCHAR(100))  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Update Task Status` (IN `ID` INT, IN `tstatus` VARCHAR(100))  BEGIN
 UPDATE tasks SET TaskStatus = tstatus WHERE TaskID = ID;
 
 END$$
 
-CREATE  PROCEDURE `updateDOC` (IN `json` LONGTEXT, IN `uID` INT)  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `updateDOC` (IN `json` LONGTEXT, IN `uID` INT)  BEGIN
 UPDATE files SET block_doc_json = json WHERE FileID = uID;
 
 END$$
 
-CREATE  PROCEDURE `updateTask` (IN `uID` INT, IN `name` VARCHAR(100), IN `deadline` DATETIME, IN `description` TEXT, IN `Tstatus` VARCHAR(100), IN `Tschedule` VARCHAR(50))  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `updateTask` (IN `uID` INT, IN `name` VARCHAR(100), IN `deadline` DATETIME, IN `description` TEXT, IN `Tstatus` VARCHAR(100), IN `Tschedule` VARCHAR(50))  BEGIN
 UPDATE tasks SET taskName  = name, TaskDeadline = deadline, TaskDescription= description, TaskStatus=Tstatus, TaskSchedule = Tschedule WHERE TaskID  = uID;
 
 END$$
 
-CREATE  PROCEDURE `updateTaskStatus` (IN `statusText` VARCHAR(100), IN `ID` INT)  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `updateTaskStatus` (IN `statusText` VARCHAR(100), IN `ID` INT)  BEGIN
 UPDATE tasks SET TaskStatus = statusText WHERE TaskID = ID;
 
 END$$
 
-CREATE  PROCEDURE `updateUser` (IN `uEmail` VARCHAR(50), IN `uFirstname` VARCHAR(50), IN `uLastName` VARCHAR(50), IN `uPhoneNumber` VARCHAR(100), IN `uPosition` INT(11), IN `uID` INT(11))  BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `updateUser` (IN `uEmail` VARCHAR(50), IN `uFirstname` VARCHAR(50), IN `uLastName` VARCHAR(50), IN `uPhoneNumber` VARCHAR(100), IN `uPosition` INT(11), IN `uID` INT(11))  BEGIN
 UPDATE users SET Email = uEmail, FirstName = uFirstname, LastName= uLastName, PhoneNumber=uPhoneNumber, Position = uPosition WHERE id = uID;
 
 END$$
@@ -222,8 +226,8 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`ID`, `FileID`, `UserID`, `DateCreated`, `CommentMsg`) VALUES
-(16, 80, 97, 1641965507, 'test\n'),
-(18, 80, 91, 1641984005, 'this is a test comment\n');
+(21, 80, 95, 1642004008, 'test3\n'),
+(25, 80, 91, 1642522591, 'test');
 
 -- --------------------------------------------------------
 
@@ -255,7 +259,8 @@ CREATE TABLE `files` (
 --
 
 INSERT INTO `files` (`FileID`, `FileName`, `FileType`, `FileSize`, `FileContentType`, `UploadedByID`, `Share_to_user`, `Share_to_group`, `DeadLine`, `Revision`, `DateUploaded`, `block_doc_json`, `status`, `pinned`, `taskID`, `FilePathName`) VALUES
-(80, 'Comments.pdf', 'raw file', 1, 'application/pdf', 91, NULL, 1, NULL, 1, '2022-01-12 00:20:55', NULL, 0, 1, 49, '91Comments.pdf');
+(80, 'Comments.pdf', 'raw file', 1, 'application/pdf', 91, NULL, 1, NULL, 1, '2022-01-12 00:20:55', NULL, 0, 1, 49, '91Comments.pdf'),
+(81, 'requirements.txt', 'raw file', 1, 'text/plain', 91, 0, 1, '2021-12-01 12:31:00', 1, '2022-01-17 21:59:49', NULL, 4, 0, 0, '91requirements.txt');
 
 -- --------------------------------------------------------
 
@@ -303,7 +308,7 @@ CREATE TABLE `tasks` (
 --
 
 INSERT INTO `tasks` (`TaskID`, `TaskName`, `TaskImage`, `TaskCreatedBy`, `TaskDateCreated`, `TaskDeadline`, `TaskDescription`, `TaskStatus`, `TaskSchedule`) VALUES
-(49, 'Individual daily log and accomplishment report (IDLAR)', '', 82, '2021-12-30 22:04:16', '2022-01-02 22:03:00', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Pending Teachers', 'Weekly'),
+(49, 'Individual daily log and accomplishment report (IDLAR)', '', 82, '2021-12-30 22:04:16', '2022-01-02 22:03:00', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Done', 'Weekly'),
 (50, 'Weekly Assesment', '', 82, '2022-01-06 22:39:41', '2022-01-16 22:39:00', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Pending Teachers', 'Weekly'),
 (51, 'Weekly home learning plan', '', 82, '2022-01-06 22:49:33', '2022-01-15 22:49:00', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Pending Teachers', 'Weekly'),
 (52, 'Idea lesson exemplar', '', 82, '2022-01-06 22:50:04', '2022-01-15 22:49:00', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', 'Pending Teachers', 'Weekly'),
@@ -336,7 +341,7 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`ID`, `Email`, `Pass`, `FirstName`, `LastName`, `PhoneNumber`, `Position`, `DateCreated`, `profilePic`) VALUES
 (82, 'admin@gmail.com', '89805776bf3dbcf0bad0bce14704d89eccde55cef2cd7cf6f068010d2ceb9480', 'admin1', 'account', '+639751160135', 1, '2021-11-18 08:36:42', '/static/uploads/ProfilePictures/82_2874.png'),
 (86, 'weconnect.thesis@gmail.com', '89805776bf3dbcf0bad0bce14704d89eccde55cef2cd7cf6f068010d2ceb9480', 'Super', '2', '01234567890', 1, '2021-11-22 00:17:06', ''),
-(91, 'teacher@gmail.com', 'cbdc324449652371c3ee4253adc7fc2c0403185a8f824d31e545a3a58db1935b', 'Teacher', 'Account', '01234567890', 4, '2021-12-21 10:27:00', '/static/uploads/ProfilePictures/91_18.jpg'),
+(91, 'teacher@gmail.com', 'cbdc324449652371c3ee4253adc7fc2c0403185a8f824d31e545a3a58db1935b', 'Teacher5', 'Account1', '01234567890', 4, '2021-12-21 10:27:00', '/static/uploads/ProfilePictures/91_18.jpg'),
 (93, 'principal@gmail.com', 'cbdc324449652371c3ee4253adc7fc2c0403185a8f824d31e545a3a58db1935b', 'Principal', 'User', '01234567890', 5, '2021-12-30 10:58:31', '/static/uploads/ProfilePictures/93_47.jpg'),
 (94, 'districtsupervisor@gmail.com', 'cbdc324449652371c3ee4253adc7fc2c0403185a8f824d31e545a3a58db1935b', 'District', 'Supervisor', '01234567890', 2, '2021-12-30 10:59:02', '/static/uploads/ProfilePictures/94_52.jpg'),
 (95, 'teacher1@gmail.com', 'cbdc324449652371c3ee4253adc7fc2c0403185a8f824d31e545a3a58db1935b', 'Teacher', 'One', '01234567890', 4, '2021-12-30 11:02:07', '/static/uploads/ProfilePictures/95_66.jpg'),
@@ -386,13 +391,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `files`
 --
 ALTER TABLE `files`
-  MODIFY `FileID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
+  MODIFY `FileID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
 
 --
 -- AUTO_INCREMENT for table `position`
